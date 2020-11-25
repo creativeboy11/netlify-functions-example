d30c3ee445229765ff2d36c50bfa60b3665ef8ca
creativeboy11 <69981603+creativeboy11@users.noreply.github.com>
2020-11-25 17:42:45 +0200

    Update hello_file.js

--- src/lambda/hello_file.js
+++ src/lambda/hello_file.js
@@ -2,16 +2,17 @@ import fetch from "node-fetch";
 
 exports.handler = (event, context, callback) => {
 
-  const { fileURL } = event.queryStringParameters
-
+  const { fileURL } = event.queryStringParameters;
+  const fileName = fileURL.split('/').pop();
   fetch(fileURL)
     .then(result => result.buffer())
     .then(body => callback(null, { 
       statusCode: 200, 
       headers: {
-        "Content-type": "application/msword"
+        'Content-type': 'application/octet-stream',
+        'Content-Disposition': 'attachment; filename=' + fileName),
       }, 
       body: body.toString('base64'),
       isBase64Encoded: true
-    }))
+    }));
 }
