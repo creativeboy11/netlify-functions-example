d6e18ccc14c5a158d5d9875e59fb284f7b1a8df1
creativeboy11 <69981603+creativeboy11@users.noreply.github.com>
2020-11-26 20:28:19 +0200

    Update hello_file.js

--- src/lambda/hello_file.js
+++ src/lambda/hello_file.js
@@ -2,9 +2,9 @@ import fetch from "node-fetch";
 
 exports.handler = (event, context, callback) => {
 
-  const { fileUrl } = event.queryStringParameters;
+  const { fileUrl, clientEmail } = event.queryStringParameters;
   const fileName = fileUrl.split('/').pop();
-  fetch(fileUrl)
+  fetch(`${fileUrl}?clientEmail=${clientEmail}`)
     .then(result => result.buffer())
     .then(body => callback(null, { 
       statusCode: 200, 
