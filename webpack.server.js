8516a41b047ef0e7af9c9bae95e132addd698bd0
creativeboy11 <69981603+creativeboy11@users.noreply.github.com>
2020-12-25 18:50:11 +0300

    Update webpack.server.js

--- webpack.server.js
+++ webpack.server.js
@@ -1,12 +1,1 @@
 require("dotenv").config();
-
-module.exports = {
-  module: {
-    rules: [
-      {
-          test: /node_modules[/\\]jsonstream/i
-          , loader: 'shebang-loader'
-      }
-    ]
-  }
-};
