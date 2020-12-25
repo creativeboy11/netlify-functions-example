9122f4f596feea30ad72a3f63851c1250284356a
creativeboy11 <69981603+creativeboy11@users.noreply.github.com>
2020-12-25 18:36:03 +0300

    Update webpack.client.js

--- webpack.client.js
+++ webpack.client.js
@@ -9,6 +9,14 @@ module.exports = {
     filename: "main.js",
     path: path.resolve("dist/client")
   },
+  module: {
+  rules: [
+    {
+        test: /node_modules[/\\]jsonstream/i,
+      loader: 'shebang-loader'
+    }
+  ]
+},
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
